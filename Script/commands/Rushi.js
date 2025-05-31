a70caae551fc16cb3b9a45fa245e69fd3e8822c8
bathuman <batman88223@gmail.com>
2025-05-31 23:34:28 +0600

    Update Rushi.js

--- Script/commands/Rushi.js
+++ Script/commands/Rushi.js
@@ -1,30 +1,36 @@
+const axios = require('axios');
+const fs = require("fs");
+const path = require("path");
+const request = require('request');
+
 module.exports.config = {
- name: "rushia",
- version: "1.0.0",
- hasPermssion: 0,
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
- description: "Random Rushia",
- commandCategory: "random-img",
- usages: "rushia",
- cooldowns: 3
+  name: "rushia",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  description: "Random Rushia image",
+  commandCategory: "random-img",
+  usages: "rushia",
+  cooldowns: 3
 };
 
 module.exports.run = async ({ api, event }) => {
- const axios = require('axios');
- const request = require('request');
- const fs = require("fs");
- axios.get('https://saikiapi-v3-production.up.railway.app/holo/rushia').then(res => {
- let ext = res.data.url.substring(res.data.url.lastIndexOf(".") + 1);
- let callback = function () {
-     api.sendMessage({
-      attachment: fs.createReadStream(__dirname + `/cache/rushia.${ext}`)
-     }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/rushia.${ext}`), event.messageID);
-   api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+  try {
+    const res = await axios.get('https://saikiapi-v3-production.up.railway.app/holo/rushia');
+    const imageUrl = res.data.url;
+    const ext = path.extname(imageUrl);
+    const filePath = path.join(__dirname, `/cache/rushia${ext}`);
+
+    const callback = () => {
+      api.sendMessage({
+        attachment: fs.createReadStream(filePath)
+      }, event.threadID, () => fs.unlinkSync(filePath), event.messageID);
+      api.setMessageReaction("✅", event.messageID, () => {}, true);
     };
-    request(res.data.url).pipe(fs.createWriteStream(__dirname + `/cache/rushia.${ext}`)).on("close", callback);
-   })
-      .catch(err => {
-                     api.sendMessage("there's something problem while generating photo, please try again!", event.threadID, event.messageID);
-    api.setMessageReaction("☹️", event.messageID, (err) => {}, true);
-                  })    
-}
+
+    request(imageUrl).pipe(fs.createWriteStream(filePath)).on("close", callback);
+  } catch (error) {
+    api.sendMessage("❌ There was a problem generating the image, please try again later.", event.threadID, event.messageID);
+    api.setMessageReaction("☹️", event.messageID, () => {}, true);
+  }
+};
